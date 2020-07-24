8adfb240c0a6c560946391160e74f2b44dfac0da
Taokoo <820031634@qq.com>
2020-07-24 13:55:03 +0800

    添加官服/wegame/360区服

--- src/main/java/com/taokoo/www/domain/po/BigRigion.java
+++ src/main/java/com/taokoo/www/domain/po/BigRigion.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -13,24 +12,22 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @ClassName: Region  
- * @Description: 区服
+  * 大区（官服，360服，wegame服）
+ * @ClassName: BigRigion  
+ * @Description: 
  * @author Taokoo
- * @date 2020-7-20
+ * @date 2020-7-24
  */
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Region {
+public class BigRigion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     private String value;
-    
-    @ManyToOne
-    private BigRigion bigRigion;
 }
