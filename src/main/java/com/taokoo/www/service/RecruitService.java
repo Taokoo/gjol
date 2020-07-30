4e792eef1674a8f09c8bba1758906848c8a2d715
Taokoo <820031634@qq.com>
2020-07-30 10:49:58 +0800

    update

--- src/main/java/com/taokoo/www/service/RecruitService.java
+++ src/main/java/com/taokoo/www/service/RecruitService.java
@@ -7,6 +7,12 @@ import com.taokoo.www.domain.vo.Result;
 @Component
 public class RecruitService {
 
+	/**
+	  * 开启一个新招募
+	 * @Title: openRecruit  
+	 * @return 
+	 * @author Taokoo
+	 */
 	public Result openRecruit() {
 		return null;
 	}
