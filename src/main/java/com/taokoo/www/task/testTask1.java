cde0493ee4c59f1b6dc04393650373c50867610a
wgy <2364005915@qq.com>
2020-09-06 15:33:38 +0800

    1,新加了一个task脚本 2,添加了几个test类 ......

--- src/main/java/com/taokoo/www/task/testTask1.java
+++ src/main/java/com/taokoo/www/task/testTask1.java
@@ -1,4 +1,15 @@
 package com.taokoo.www.task;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class testTask1 {
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void handle(){
+       System.out.println("定时输出叫伯恩");
+    }
 }
